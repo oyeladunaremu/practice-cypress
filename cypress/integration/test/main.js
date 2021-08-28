a3d3c0461f89d9aa04742a88120bee74b68059ad
Oyeladun Aremu <oyeladun.aremu@wakanow.com>
2021-08-28 22:36:15 +0100

    cucumber installed

--- cypress/integration/test/main.js
+++ cypress/integration/test/main.js
@@ -1,4 +1,4 @@
-import PageActions from '../../support/PageResources/pageActions'
+import PageActions from '../../support/PageResource/pageActions'
 
 const pages = new PageActions();
 
@@ -11,14 +11,15 @@ context('Web App Security site', ()=>{
   it('Test Valid Signin on Webappsecurity site', () => {
     //Make a search using the word "pay"
     cy.fixture("elementMapper").then((em)=>{
-    //cy.get(em.searchBox).type(em.searchItem);
-    pages.insertValue(em.searchBox, em.searchItem);
-    cy.get(em.signinButton).click();
-    cy.get(em.loginBox).type(em.userName);
-    cy.get(em.passwordBox).type(em.passWord);
+    cy.get(em.searchBox).type(em.searchItem);
+    //pages.insertValue(em.searchBox, em.searchItem);
+    pages.clickElement(em.signinButton);
+    pages.typeAValue(em.loginBox, em.userName);
+    pages.typeAValue(em.passwordBox, em.passwordBox);
     cy.wait(3000)
     //cy.get('input#user_remember_me').click();
-    cy.get(em.signIn).click();
+    //cy.get(em.signIn).click();
+    pages.clickElement(em.signIn);
 
     })
 
@@ -27,9 +28,10 @@ context('Web App Security site', ()=>{
 it('Test Invalid Signin on Webappsecurity site', () => {
   cy.fixture("elementMapper").then((em)=>{
     cy.get(em.searchBox).type(em.searchItem);
-    cy.get(em.signinButton).click();
-    cy.get(em.loginBox).type(em.userName);
-    cy.get(em.passwordBox).type(em.invalidPassword);
+    pages.clickElement(em.signinButton);
+    pages.typeAValue(em.loginBox, em.userName);
+    //cy.get(em.passwordBox).type(em.invalidPassword);
+    pages.typeAValue(em.passwordBox, em.invalidPassword);
     cy.wait(3000)
     cy.get(em.signIn).click();
 
